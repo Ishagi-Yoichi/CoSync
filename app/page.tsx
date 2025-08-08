b104105b4b4c6295d29cb830972d080f401c410c
Ishagi-Yoichi <nikunjkr1752003@gmail.com>
2025-08-08 23:55:03 +0530

    Protected pages(Home)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,29 @@
+"use client"
 import AppBar from "@/components/AppBar";
 import { CodeBlock } from "@/components/code-block";
 import { Cover } from "@/components/Cover";
 import Hero from "@/components/Hero";
+import { getServerSession } from "next-auth";
 import Image from "next/image";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
 
 
-export default function Home() {
+export default async function Home() {
+  const { data:session , status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if(status ==="unauthenticated"){
+      router.push("/signin");
+    }
+  },[status,router]);
+  if(status ==="loading") return <div>Loading.....</div>;
+  toast.success("Signed Up Successfully");
   return (
    <div className="bg-black min-h-screen ">
     
