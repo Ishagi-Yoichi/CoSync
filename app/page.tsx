861924c03a1e96bd46e01a45b7902ad06357ee26
Ishagi-Yoichi <nikunjkr1752003@gmail.com>
2025-08-09 00:11:22 +0530

    Protected page issue resolved

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+
 import AppBar from "@/components/AppBar";
 import { CodeBlock } from "@/components/code-block";
 import { Cover } from "@/components/Cover";
@@ -6,24 +6,18 @@ import Hero from "@/components/Hero";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import { useState } from "react";
+//import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { redirect } from "next/navigation";
+//import { useEffect } from "react";
 import toast from "react-hot-toast";
 
 
 export default async function Home() {
-  const { data:session , status } = useSession();
-  const router = useRouter();
-
-  useEffect(() => {
-    if(status ==="unauthenticated"){
-      router.push("/signin");
-    }
-  },[status,router]);
-  if(status ==="loading") return <div>Loading.....</div>;
-  toast.success("Signed Up Successfully");
+  const  session   = await getServerSession(authOptions);
+ if(!session){
+  redirect("/signin");
+ }
   return (
    <div className="bg-black min-h-screen ">
     
