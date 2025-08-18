35df22a6492aef5c1542427dd401ad39361548a0
Ishagi-Yoichi <nikunjkr1752003@gmail.com>
2025-08-19 00:29:24 +0530

    Signin UI update

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -3,6 +3,8 @@
 import { useState } from 'react';
 import { signIn } from 'next-auth/react';
 import { useRouter, useSearchParams } from 'next/navigation';
+import { Geist } from 'next/font/google';
+const geist = Geist({ subsets: ['latin'] });
 
 export default function SignInPage() {
   const router = useRouter();
@@ -31,31 +33,46 @@ export default function SignInPage() {
   }
 
   return (
-    <div style={{ maxWidth: '400px', margin: '50px auto' }}>
-      <h2>Sign In</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          className='text-white border border-b-white'
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <br />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          className='text-white'
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <br />
-        <button type="submit" className='bg-blue-600'>Sign In</button>
-      </form>
+    <div className="h-screen flex justify-center items-center bg-pink-50">
+      <div className="block max-w-sm p-6 bg-white border border-pink-200 rounded-2xl shadow">
+        <div className={`text-3xl font-bold mb-4 text-center ${geist.className}`}>
+          Sign In
+        </div>
+        <form onSubmit={handleSubmit} className="flex flex-col space-y-4">
+          <label>
+            Email:
+            <input
+              type="email"
+              className="w-full mt-1 p-2 border rounded"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="nik22@gmail.com"
+              required
+            />
+          </label>
 
-      {error && <p style={{ color: 'red' }}>{error}</p>}
+          <label>
+            Password:
+            <input
+              type="password"
+              className="w-full mt-1 p-2 border rounded"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="••••••••"
+              required
+            />
+          </label>
+
+          {error && <p className="text-red-500 text-sm">{error}</p>}
+
+          <button
+            type="submit"
+            className={`mt-4 w-full text-white bg-pink-500 hover:bg-pink-600 focus:ring-4 focus:ring-gray-300 font-medium rounded-3xl text-sm px-5 py-2.5 ${geist.className}`}
+          >
+            Sign In
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
