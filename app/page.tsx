671ed12dfb2969a22ccc118cdbf67f06ea73be6e
Ishagi-Yoichi <nikunjkr1752003@gmail.com>
2026-05-21 11:05:40 +0530

    Audio feat-02

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,17 @@
-
 import AppBar from "@/components/AppBar";
 import Hero from "@/components/Hero";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth-options";
 import HeroBackground from "@/components/HeroBackground";
-
+import { authOptions } from "@/lib/auth-options";
+import { getServerSession } from "next-auth";
 
 export default async function Home() {
   await getServerSession(authOptions);
- 
+
   return (
-   <div className="bg-black min-h-screen ">
-    <HeroBackground/>
-    <AppBar/>
-    
-    <Hero/>
-   
-   </div>
-  )
+    <div className="premium-shell relative min-h-screen overflow-hidden">
+      <HeroBackground />
+      <AppBar />
+      <Hero />
+    </div>
+  );
 }
